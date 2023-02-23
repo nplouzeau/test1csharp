69d9ca0f3514b584c6c7d95f7d4a7c67fce04ea7
NP <noelplouzeau@protonmail.com>
2023-02-23 16:04:32 +0100

    Depuis VS 2022 Windows

--- test1csharp/Program.cs
+++ test1csharp/Program.cs
@@ -1,4 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using System;
+
 Console.WriteLine("Salut, monde !");
+Console.WriteLine("Hello again!");
 Console.WriteLine("Hello again!");
 
